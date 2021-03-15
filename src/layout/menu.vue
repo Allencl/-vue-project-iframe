e9398cd794d5b3f446c1668feee942973db79c39
Allencl <Allen__cc@163.com>
2021-03-15 10:32:56 +0800

    1

---
+++ src/layout/menu.vue
@@ -0,0 +1,64 @@
+<template>
+    <div style="height:100%">
+        <Menu
+            width="auto"
+            style="height:100%"
+            @on-select="selectHandle"
+        >
+            <menu-sun :menus="menuList"/>
+        </Menu>
+    </div>
+</template>
+<script>
+import MenuSun from "@src/layout/menuList.vue";
+export default {
+    components: {
+        MenuSun
+    },
+    data() {
+        return {
+            // 菜单 数据
+            menuList: [
+                {
+                    name: "一级菜单",
+                    id: "1",
+                    children: [
+                        {
+                            id:"1-1",
+                            name: "表单示例", 
+                            icon:"logo-buffer",
+                            pathName:"page1"            
+                        },
+                        {
+                            name: "二级菜单",
+                            id: "1-2",
+                            children: [
+                                {
+                                    name: "跳转示例1", 
+                                    id: "1-2-1",
+                                    icon:"ios-analytics",
+                                    pathName:"page2"            
+                                }
+                            ]
+                        },
+                        {
+                            id:"1-3",
+                            name: "跳转示例2", 
+                            icon:"logo-buffer",
+                            pathName:"page3"            
+                        },
+                    ]
+                }
+            ]
+        };
+    },
+    methods:{
+        /**
+         * 菜单 点击
+         */
+        selectHandle: function(option){
+            this.$emit("addMenuHandle",option);
+        }
+    }
+};
+</script>
